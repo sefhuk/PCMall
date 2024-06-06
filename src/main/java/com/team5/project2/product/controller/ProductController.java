46c2d6993b6a2668edbb20df6724f678db54ac39
sanghyuk <ish775800@gmail.com>
2024-06-06 20:26:15 +0900

    feat: 상품 등록 Controller

--- src/main/java/com/team5/project2/product/controller/ProductController.java
+++ src/main/java/com/team5/project2/product/controller/ProductController.java
@@ -1,14 +1,19 @@
 package com.team5.project2.product.controller;
 
-import com.team5.project2.product.dto.request.ProductRequestDto;
+import com.team5.project2.category.dto.CategoryDTO;
+import com.team5.project2.category.service.CategoryService;
+
+import com.team5.project2.product.dto.response.SelectOptionDto;
 import com.team5.project2.product.entity.Product;
+import com.team5.project2.product.mapper.ProductMapper;
 import com.team5.project2.product.service.ProductService;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +25,48 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/product")
 @RequiredArgsConstructor
-@Slf4j
 public class ProductController {
 
     private final ProductService productService;
+    private final CategoryService categoryService;
+    private final ProductMapper productMapper;
 
     @GetMapping
-    public String getProductSavePage() {
+    public String getProductSavePage(Model model) {
+
+        List<CategoryDTO> parts = categoryService.getAllCategories();
+
+        List<SelectOptionDto> options = new ArrayList<>();
+        for (CategoryDTO category : parts) {
+            for (CategoryDTO subCategory : categoryService.getSubCategories(category.getId())) {
+                SelectOptionDto option = productMapper.CategoryDtoToSelectOptionDto(
+                    subCategory);
+
+                if (subCategory.getParentId() == 1L) {
+                    option.setClassAttribute("cpu");
+                } else if (subCategory.getParentId() == 2L) {
+                    option.setClassAttribute("cooler");
+                } else if (subCategory.getParentId() == 3L) {
+                    option.setClassAttribute("mainboard");
+                } else if (subCategory.getParentId() == 4L) {
+                    option.setClassAttribute("ram");
+                } else if (subCategory.getParentId() == 5L) {
+                    option.setClassAttribute("gpu");
+                } else if (subCategory.getParentId() == 6L) {
+                    option.setClassAttribute("ssd");
+                } else if (subCategory.getParentId() == 7L) {
+                    option.setClassAttribute("hdd");
+                } else {
+                    option.setClassAttribute("case");
+                }
+
+                options.add(option);
+            }
+        }
+
+        model.addAttribute("parts", parts);
+        model.addAttribute("options", options);
+
         return "product/product-create";
     }
 
@@ -45,6 +85,8 @@ public class ProductController {
             .description(description)
             .build();
 
-        return ResponseEntity.ok(productService.addProduct(newProduct, part, images));
+        productService.addProduct(newProduct, part, images);
+
+        return ResponseEntity.ok().build();
     }
 }
