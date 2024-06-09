ead7c7f1f0c50c02e161a49873cf332a4ececba2
sanghyuk <ish775800@gmail.com>
2024-06-09 16:52:42 +0900

    feat: ProductResponseDto 어노테이션 일부 추가, description 필드 타입 변경

--- src/main/java/com/team5/project2/product/dto/response/ProductResponseDto.java
+++ src/main/java/com/team5/project2/product/dto/response/ProductResponseDto.java
@@ -2,6 +2,9 @@ package com.team5.project2.product.dto.response;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +12,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductResponseDto {
     private Long id;
     private String part;
@@ -17,5 +22,5 @@ public class ProductResponseDto {
     private Long stock;
     private Long price;
     private List<ProductImageResponseDto> images = new ArrayList<>();
-    private String description;
+    private Map<String, String> description;
 }
