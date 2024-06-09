f7d78d6f23f55dac91a57f60e6886dfe67ac1609
sanghyuk <ish775800@gmail.com>
2024-06-09 16:54:03 +0900

    feat: ProductMapper 인터페이스 내부에 구현체 INSTANCE 필드 추가, images 필드 커스텀 매핑 메소드 추가

--- src/main/java/com/team5/project2/product/mapper/ProductMapper.java
+++ src/main/java/com/team5/project2/product/mapper/ProductMapper.java
@@ -1,19 +1,31 @@
 package com.team5.project2.product.mapper;
 
 import com.team5.project2.category.dto.CategoryDTO;
+import com.team5.project2.product.dto.response.ProductImageResponseDto;
 import com.team5.project2.product.dto.response.ProductResponseDto;
 import com.team5.project2.product.dto.response.SelectOptionDto;
 import com.team5.project2.product.entity.Product;
+import com.team5.project2.product.entity.ProductImage;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductMapper {
 
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
     SelectOptionDto CategoryDtoToSelectOptionDto(CategoryDTO categoryDTO);
 
-    @Mapping(target = "images", ignore = true)
+    @Mapping(target = "images", expression = "java(mapImages(product.getImages()))")
     @Mapping(source = "category.name", target = "part")
     ProductResponseDto productToProductResponseDto(Product product);
+
+    default List<ProductImageResponseDto> mapImages(List<ProductImage> images) {
+        return images.stream().map(ProductImageMapper.INSTANCE::productImageToProductImageResponseDto)
+            .collect(Collectors.toList());
+    }
 }
