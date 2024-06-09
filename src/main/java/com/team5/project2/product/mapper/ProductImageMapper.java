134902cd2d603d1e5affc0fb8f44155a22050900
sanghyuk <ish775800@gmail.com>
2024-06-09 16:55:43 +0900

    feat: ProductImageMapper 인터페이스 내부에 구현체 INSTANCE 필드 추가

--- src/main/java/com/team5/project2/product/mapper/ProductImageMapper.java
+++ src/main/java/com/team5/project2/product/mapper/ProductImageMapper.java
@@ -4,8 +4,12 @@ import com.team5.project2.product.dto.response.ProductImageResponseDto;
 import com.team5.project2.product.entity.ProductImage;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductImageMapper {
+
+    ProductImageMapper INSTANCE = Mappers.getMapper(ProductImageMapper.class);
+
     ProductImageResponseDto productImageToProductImageResponseDto(ProductImage productImage);
 }
