c8ec15448e8c34d2b4710f0993eea5f340c58f8d
seungooan <wa594@naver.com>
2024-06-14 22:23:03 +0900

    유저 수정 페이지 완료

---
+++ src/main/generated/com/team5/project2/user/mapper/UserMapperImpl.java
@@ -0,0 +1,48 @@
+package com.team5.project2.user.mapper;
+
+import com.team5.project2.user.domain.User;
+import com.team5.project2.user.dto.UserPostDto;
+import com.team5.project2.user.dto.UserPutDto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-06-14T17:03:53+0900",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User userPostDtoToUser(UserPostDto userPostDto) {
+        if ( userPostDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setName( userPostDto.getName() );
+        user.setEmail( userPostDto.getEmail() );
+        user.setPassword( userPostDto.getPassword() );
+        user.setPhone_number( userPostDto.getPhone_number() );
+
+        return user;
+    }
+
+    @Override
+    public User userPutDtoToUser(UserPutDto userPutDto) {
+        if ( userPutDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setName( userPutDto.getName() );
+        user.setEmail( userPutDto.getEmail() );
+        user.setPassword( userPutDto.getPassword() );
+        user.setAddress( userPutDto.getAddress() );
+
+        return user;
+    }
+}
