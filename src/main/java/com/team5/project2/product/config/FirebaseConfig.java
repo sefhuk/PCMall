38f0082828d916d50bf89ad4f00d80f5fdd18e14
sefhuk <ish0411@naver.com>
2024-06-14 19:21:58 +0900

    chore: key file 제거, config 반영

--- src/main/java/com/team5/project2/product/config/FirebaseConfig.java
+++ src/main/java/com/team5/project2/product/config/FirebaseConfig.java
@@ -8,9 +8,9 @@ import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.StorageClient;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,7 +20,8 @@ public class FirebaseConfig {
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
 
-        FileInputStream stream = new FileInputStream("src/main/java/com/team5/project2/product/config/firebaseKey.json");
+        InputStream stream = getClass().getResourceAsStream("/firebaseKey.json");
+        assert stream != null;
         FirebaseOptions options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(stream))
             .setStorageBucket("elice-project2.appspot.com")
@@ -37,7 +38,8 @@ public class FirebaseConfig {
     @Bean
     public Storage storage() throws IOException {
         StorageOptions storageOptions = StorageOptions.newBuilder()
-            .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("src/main/java/com/team5/project2/product/config/firebaseKey.json")))
+            .setCredentials(ServiceAccountCredentials.fromStream(
+                Objects.requireNonNull(getClass().getResourceAsStream("/firebaseKey.json"))))
             .build();
         return storageOptions.getService();
     }
