119b9ffe9a6504904d31dddef8b943223fa1fbf3
HyeonTee <gusxo1820@icloud.com>
2024-06-20 16:00:27 +0900

    Admin order pagination

--- src/main/java/com/team5/project2/order/controller/AdminOrderController.java
+++ src/main/java/com/team5/project2/order/controller/AdminOrderController.java
@@ -7,6 +7,9 @@ import com.team5.project2.user.service.UserService;
 import java.security.Principal;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +23,11 @@ public class AdminOrderController {
     private final OrderService orderService;
 
     @GetMapping
-    public String adminHome(Model model) {
-        List<OrderDto> orders = orderService.getAllOrders();
+    public String adminHome(Model model, @PageableDefault(size = 10) Pageable pageable) {
+        Page<OrderDto> orders = orderService.getAllOrders(pageable);
         model.addAttribute("orders", orders);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("totalPages", orders.getTotalPages());
         return "order/adminHome";
     }
 }
