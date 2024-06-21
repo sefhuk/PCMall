1d39d59eda4d04111e22e4ff3baf9fd456536404
sanghyuk <ish775800@gmail.com>
2024-06-21 14:49:45 +0900

    refactor: 불필요 코드 정리

--- src/main/java/com/team5/project2/product/repository/ProductRepository.java
+++ src/main/java/com/team5/project2/product/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.team5.project2.product.repository;
 
 import com.team5.project2.product.entity.Product;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -10,9 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    @EntityGraph(attributePaths = {"category", "images"})
-    List<Product> findByCategoryId(Long categoryId);
 
     @EntityGraph(attributePaths = {"category", "images"})
     Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
