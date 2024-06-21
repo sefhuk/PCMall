09d4abc547a886add69006b13a4b2295741245dd
seungooan <wa594@naver.com>
2024-06-21 20:10:18 +0900

    카트 삭제 수정

--- src/main/java/com/team5/project2/cart/controller/CartController.java
+++ src/main/java/com/team5/project2/cart/controller/CartController.java
@@ -4,10 +4,14 @@ import com.team5.project2.cart.dto.CartDTO;
 import com.team5.project2.cart.service.CartService;
 import com.team5.project2.user.domain.User;
 import com.team5.project2.user.service.UserService;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +21,7 @@ public class CartController {
 
     private final CartService cartService;
     private final UserService userService;
+    private final UserDetailsService userDetailsService;
 
     @GetMapping
     public ResponseEntity<CartDTO> getCart() {
@@ -40,9 +45,11 @@ public class CartController {
     }
 
     @DeleteMapping("/remove")
-    public ResponseEntity<Void> removeCartItem(@RequestParam Long itemId) {
+    public ResponseEntity<Void> removeCartItem(@RequestParam Long itemId, Principal principal) {
+        String userEmail = principal.getName();
         Long userId = getCurrentUserId();
         cartService.removeCartItem(userId, itemId);
+        updateSecurityContext(userEmail);
         return ResponseEntity.ok().build();
     }
 
@@ -61,5 +68,11 @@ public class CartController {
             return user.getId();
         }
         throw new RuntimeException("사용자 인증 정보가 없습니다.");
+    }
+
+    private void updateSecurityContext(String username) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
