066d29477438a4c55bff37ccdd407327c6a489c4
sanghyuk <ish775800@gmail.com>
2024-06-21 23:10:14 +0900

    feat: 엔터로 검색 버튼 클릭 이벤트

--- src/main/resources/static/product/js/product-list.js
+++ src/main/resources/static/product/js/product-list.js
@@ -2,6 +2,13 @@ const searchContent = document.getElementById("search-content");
 const searchCategory = document.getElementById("search-category");
 const searchButton = document.getElementById("search-button");
 
+searchContent.addEventListener("keydown", (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    searchButton.click();
+  }
+});
+
 searchButton.addEventListener("click", () => {
   location.href = `/user/product?category=${category}&searchType=${searchCategory.value}&search=${searchContent.value}`;
 });
